0822df3f94b8a796c0ac4aabccf5759302c5aab3
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-11-07 10:39:20 -0500

    initial

---
+++ pw/example.spec.js
@@ -0,0 +1,9 @@
+// @ts-check
+const { test, expect } = require('@playwright/test')
+
+test('has title', async ({ page }) => {
+  await page.goto('http://localhost:3000/')
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle('cy-vs-pw-example-todomvc')
+})

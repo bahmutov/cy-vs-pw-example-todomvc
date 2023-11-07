a31e824627cc9cb0d1c9a416a013016f904b7af9
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-11-07 10:39:58 -0500

    remove fixture and support folders

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,8 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     video: true,
+    supportFile: false,
+    fixturesFolder: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },

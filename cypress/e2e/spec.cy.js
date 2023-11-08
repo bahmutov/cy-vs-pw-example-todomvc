b59a6e5ed98e86936a94f8b3f0e709a83b2e26e5
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-11-08 16:57:59 -0500

    a6 code

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -8,9 +8,6 @@
 // see:
 // https://on.cypress.io/writing-and-organizing-tests#Hooks
 // https://on.cypress.io/request
-beforeEach(() => {
-  cy.request('POST', 'http://localhost:3000/reset', { todos: [] })
-})
 
 it('has title', () => {
   // visit the page "localhost:3000"
@@ -21,13 +18,12 @@ it('has title', () => {
   // and the body element has CSS class "loaded"
   // https://on.cypress.io/get
   // https://on.cypress.io/assertions
-  cy.get('body').should('have.class', 'loaded')
+  //
   // alternative: use the built-in existence assertion
-  cy.get('body.loaded')
+  // cy.get('body.loaded')
 
   // confirm there are 0 todo items on the page
   // use the CSS selector ".todo-list li"
   // https://on.cypress.io/get
   // https://on.cypress.io/assertions
-  cy.get('.todo-list li').should('have.length', 0)
 })
--- pw/example.spec.js
+++ pw/example.spec.js
@@ -8,9 +8,6 @@ const { test, expect } = require('@playwright/test')
 // see:
 // https://playwright.dev/docs/api/class-test
 // https://playwright.dev/docs/api-testing
-test.beforeEach(async ({ request }) => {
-  request.post('http://localhost:3000/reset', { data: { todos: [] } })
-})
 
 test('has title', async ({ page }) => {
   await page.goto('http://localhost:3000/')
@@ -18,12 +15,10 @@ test('has title', async ({ page }) => {
   // confirm the page has finished loading todos
   // and the body element has CSS class "loaded"
   // https://playwright.dev/docs/api/class-locatorassertions
-  await expect(page.locator('body')).toHaveClass('loaded')
-
+  //
   // confirm there are 0 todo items on the page
   // use the CSS selector ".todo-list li"
   // https://playwright.dev/docs/locators
   // and the count assertion
   // https://playwright.dev/docs/api/class-locatorassertions
-  await expect(page.locator('.todo-list li')).toHaveCount(0)
 })

12bf24a38a566701b13de9b2ee2219c9b7a5acf6
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-11-08 15:31:34 -0500

    pw solution

--- pw/example.spec.js
+++ pw/example.spec.js
@@ -1,18 +1,29 @@
 // @ts-check
 const { test, expect } = require('@playwright/test')
 
+// before each test clear all todos
+// by making a POST request to "/reset" endpoint
+// and pass an object { todos: [] }
+// which should clear all existing todos
+// see:
+// https://playwright.dev/docs/api/class-test
+// https://playwright.dev/docs/api-testing
+test.beforeEach(async ({ request }) => {
+  request.post('/reset', { data: { todos: [] } })
+})
+
 test('has title', async ({ page }) => {
-  await page.goto('http://localhost:3000/')
+  await page.goto('/')
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle('cy-vs-pw-example-todomvc')
+  // confirm the page has finished loading todos
+  // and the body element has CSS class "loaded"
+  // https://playwright.dev/docs/api/class-locatorassertions
+  await expect(page.locator('body')).toHaveClass('loaded')
 
-  // confirm there are 3 todo items on the page
+  // confirm there are 0 todo items on the page
   // use the CSS selector ".todo-list li"
   // https://playwright.dev/docs/locators
   // and the count assertion
   // https://playwright.dev/docs/api/class-locatorassertions
-  await expect(page.locator('.todo-list li')).toHaveCount(3, {
-    timeout: 1000,
-  })
+  await expect(page.locator('.todo-list li')).toHaveCount(0)
 })

177db706b30b35e40f06d01beacbdbcc4e4ad788
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-11-08 21:57:10 -0500

    a7 solution

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,33 +1,15 @@
 // @ts-check
 /// <reference types="cypress" />
 
-// before each test clear all todos
-// by making a POST request to "/reset" endpoint
-// and pass an object { todos: [] }
-// which should clear all existing todos
-// see:
-// https://on.cypress.io/writing-and-organizing-tests#Hooks
-// https://on.cypress.io/request
+// commands operate with respect to the "baseUrl"
+// defined in the cypress.config.js file
+
 beforeEach(() => {
-  cy.request('POST', 'http://localhost:3000/reset', { todos: [] })
+  cy.request('POST', '/reset', { todos: [] })
 })
 
 it('has title', () => {
-  // visit the page "localhost:3000"
-  // https://on.cypress.io/visit
-  cy.visit('http://localhost:3000/')
-
-  // confirm the page has finished loading todos
-  // and the body element has CSS class "loaded"
-  // https://on.cypress.io/get
-  // https://on.cypress.io/assertions
-  cy.get('body').should('have.class', 'loaded')
-  // alternative: use the built-in existence assertion
+  cy.visit('/')
   cy.get('body.loaded')
-
-  // confirm there are 0 todo items on the page
-  // use the CSS selector ".todo-list li"
-  // https://on.cypress.io/get
-  // https://on.cypress.io/assertions
   cy.get('.todo-list li').should('have.length', 0)
 })
--- pw/example.spec.js
+++ pw/example.spec.js
@@ -1,29 +1,15 @@
 // @ts-check
 const { test, expect } = require('@playwright/test')
 
-// before each test clear all todos
-// by making a POST request to "/reset" endpoint
-// and pass an object { todos: [] }
-// which should clear all existing todos
-// see:
-// https://playwright.dev/docs/api/class-test
-// https://playwright.dev/docs/api-testing
+// commands operate with respect to the "baseUrl"
+// defined in the playwright.config.js file
+
 test.beforeEach(async ({ request }) => {
-  request.post('http://localhost:3000/reset', { data: { todos: [] } })
+  request.post('/reset', { data: { todos: [] } })
 })
 
 test('has title', async ({ page }) => {
-  await page.goto('http://localhost:3000/')
-
-  // confirm the page has finished loading todos
-  // and the body element has CSS class "loaded"
-  // https://playwright.dev/docs/api/class-locatorassertions
+  await page.goto('/')
   await expect(page.locator('body')).toHaveClass('loaded')
-
-  // confirm there are 0 todo items on the page
-  // use the CSS selector ".todo-list li"
-  // https://playwright.dev/docs/locators
-  // and the count assertion
-  // https://playwright.dev/docs/api/class-locatorassertions
   await expect(page.locator('.todo-list li')).toHaveCount(0)
 })

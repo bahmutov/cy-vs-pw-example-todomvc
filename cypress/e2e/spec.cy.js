6f011cbd5a87188c20014e6e807ccbf52ee704af
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-11-13 22:21:00 -0500

    solution

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -8,24 +8,14 @@ beforeEach(() => {
   cy.request('POST', '/reset', { todos: [] })
 })
 
-// Tip: look at the following commands before writing this test
-// https://on.cypress.io/visit
-// https://on.cypress.io/get
-// https://on.cypress.io/type
-it('has title', () => {
+it('adding todos', () => {
+  // reuse the same CSS selectors
   const input = '[placeholder="What needs to be done?"]'
   const todos = '.todo-list li label'
 
-  // visit the application
   cy.visit('/')
-  // wait for the body.loaded element to be visible
   cy.get('body.loaded').should('be.visible')
-  // there should be zero todo items
   cy.get(todos).should('not.exist')
-  // find the input element using the placeholder text
-  // and type "Write code" followed by "Enter" press
   cy.get(input).type('Write code{enter}')
-  // there should be one todo item on the page
-  // the todo item label should have the entered text
   cy.get(todos).should('have.length', 1).and('have.text', 'Write code')
 })
--- pw/example.spec.js
+++ pw/example.spec.js
@@ -6,25 +6,15 @@ test.beforeEach(async ({ request }) => {
   request.post('/reset', { data: { todos: [] } })
 })
 
-// Tip: read the "Actions" Guide before implementing this test
-// https://playwright.dev/docs/input
 test('adding todos', async ({ page }) => {
+  // avoid duplicator code by reusing the same locator objects
   const input = page.getByPlaceholder('What needs to be done?')
   const todos = page.locator('.todo-list li label')
 
-  // visit the application
   await page.goto('/')
-  // wait for the body.loaded element to be visible
-  await expect(page.locator('body.loaded')).toBeVisible()
-  // there should be zero todo items
+  await page.locator('body.loaded').waitFor()
   await expect(todos).toHaveCount(0)
-  // find the input element using the placeholder text
-  // and type "Write code" followed by "Enter" press
-
   await input.fill('Write code')
   await input.press('Enter')
-
-  // there should be one todo item on the page
-  // the todo item label should have the entered text
   await expect(todos).toHaveText(['Write code'])
 })

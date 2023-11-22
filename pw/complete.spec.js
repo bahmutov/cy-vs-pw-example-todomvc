22f8d77ae1641fc5c602e831574fe7f55a204bc0
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-11-22 08:58:35 -0500

    b3 lesson

--- pw/complete.spec.js
+++ pw/complete.spec.js
@@ -18,36 +18,13 @@ test.describe('Complete todos', () => {
 
     // enter three todos
     // "Write code", "Write tests", "Make tests pass"
-    await input.fill('Write code')
-    await input.press('Enter')
-    await input.fill('Write tests')
-    await input.press('Enter')
-    await input.fill('Make tests pass')
-    await input.press('Enter')
-
     // confirm the item labels
-    await expect(todoLabels).toHaveText([
-      'Write code',
-      'Write tests',
-      'Make tests pass',
-    ])
     // confirm the "3" todos remaining is shown
-    await expect(count).toHaveText('3')
-
     // complete the first item by clicking its toggle element
-    await todos.first().locator('.toggle').click()
     // the first item should have class "completed"
-    await expect(todos.first()).toHaveClass(/completed/)
     // confirm the 2nd and the 3rd items do not have class "completed"
-    await expect(todos.nth(1)).not.toHaveClass(/completed/)
-    await expect(todos.nth(2)).not.toHaveClass(/completed/)
     // Bonus: can you confirm classes for all 3 elements at once
-    await expect(todos).toHaveClass(['todo completed', 'todo', 'todo'])
     // confirm there are 2 remaining items
-    await expect(count).toHaveText('2')
     // and that we can clear the completed items button appears
-    await expect(
-      page.getByRole('button', { name: 'Clear completed' }),
-    ).toBeVisible()
   })
 })

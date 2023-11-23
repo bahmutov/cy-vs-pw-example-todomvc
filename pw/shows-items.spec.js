2080e5f8e015c1db1d12d9e86af6541efd1aff50
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-11-23 10:44:52 -0500

    initial code

--- pw/shows-items.spec.js
+++ pw/shows-items.spec.js
@@ -6,15 +6,7 @@ test.describe('App', () => {
   test.beforeEach(async ({ request }) => {
     // confirm there are several items
     // and some are completed and some are not
-    expect(items.length, 'number of items').toBeGreaterThan(0)
-    expect(
-      items.some((item) => item.completed),
-      'has completed items',
-    ).toBeTruthy()
-    expect(
-      items.some((item) => !item.completed),
-      'has incomplete items',
-    ).toBeTruthy()
+
     request.post('/reset', { data: { todos: items } })
   })
 
@@ -26,22 +18,11 @@ test.describe('App', () => {
     await page.goto('/')
 
     // shows N items
-    await expect(todos).toHaveCount(items.length)
+
     // go through the items and confirm each is rendered correctly
     // - label text
     // - completed or not
-    for (const [k, item] of items.entries()) {
-      const itemLocator = todos.nth(k)
-      await expect(itemLocator.locator('label')).toHaveText(item.title)
-      if (item.completed) {
-        await expect(itemLocator).toHaveClass(/completed/)
-      } else {
-        await expect(itemLocator).not.toHaveClass(/completed/)
-      }
-    }
 
     // confirm the remaining items count is correct
-    const n = items.filter((item) => !item.completed).length
-    await expect(count).toHaveText(String(n))
   })
 })

dccddb27a3636775d475fc20276818cd57abdf50
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-11-27 13:25:42 -0500

    b7 code

--- cypress/e2e/delete-items.cy.js
+++ cypress/e2e/delete-items.cy.js
@@ -14,24 +14,10 @@ describe('App', () => {
 
     cy.visit('/')
     cy.get(todos).should('have.length', 3)
+
     // delete one completed item (the middle one)
-    cy.get(todos)
-      .eq(1)
-      .find('.destroy')
-      // skip visibility check
-      .click({ force: true })
     // confirm the remaining two items are still there
-    cy.get(todos)
-      .should('have.length', 2)
-      .then(($li) => Cypress._.map($li, 'innerText'))
-      .should('deep.equal', ['Write code', 'Make tests pass'])
     // delete one incomplete item (the first one)
-    cy.get(todos)
-      .first()
-      .find('.destroy')
-      // skip visibility check
-      .click({ force: true })
     // confirm the one remaining item
-    cy.get(todos).should('have.length', 1).contains('Make tests pass')
   })
 })
--- pw/delete-items.spec.js
+++ pw/delete-items.spec.js
@@ -12,17 +12,10 @@ test.describe('App', () => {
     const todos = page.locator('.todo-list li')
     await page.goto('/')
     await expect(todos).toHaveCount(3)
+
     // delete one completed item (the middle one)
-    await todos.nth(1).hover()
-    await todos.nth(1).locator('.destroy').click()
     // confirm the remaining two items are still there
-    await expect(todos).toHaveCount(2)
-    await expect(todos).toHaveText(['Write code', 'Make tests pass'])
     // delete one incomplete item (the first one)
-    await todos.nth(0).hover()
-    await todos.nth(0).locator('.destroy').click()
     // confirm the one remaining item
-    await expect(todos).toHaveCount(1)
-    await expect(todos).toHaveText(['Make tests pass'])
   })
 })

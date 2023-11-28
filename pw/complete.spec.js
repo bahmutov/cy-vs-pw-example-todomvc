6aa16d7088731ec923e2fc1e9bb903efbfe2afaa
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-11-28 10:36:40 -0500

    await

--- pw/complete.spec.js
+++ pw/complete.spec.js
@@ -3,7 +3,7 @@ const { test, expect } = require('@playwright/test')
 
 test.describe('Complete todos', () => {
   test.beforeEach(async ({ request }) => {
-    request.post('/reset', { data: { todos: [] } })
+    await request.post('/reset', { data: { todos: [] } })
   })
 
   test('completes a todo', async ({ page }) => {

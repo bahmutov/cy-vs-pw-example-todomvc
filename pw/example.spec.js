f279c6e3dc06998ef202b61a5d39919103bcee34
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-11-28 10:36:12 -0500

    await

--- pw/example.spec.js
+++ pw/example.spec.js
@@ -3,7 +3,7 @@ const { test, expect } = require('@playwright/test')
 
 // start each test with zero todos
 test.beforeEach(async ({ request }) => {
-  request.post('/reset', { data: { todos: [] } })
+  await request.post('/reset', { data: { todos: [] } })
 })
 
 // Tip: read the "Actions" Guide before implementing this test

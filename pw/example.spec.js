883a2726131e00f2c2b9e14add61f64446bba52b
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-11-28 10:35:51 -0500

    await

--- pw/example.spec.js
+++ pw/example.spec.js
@@ -5,7 +5,7 @@ const { test, expect } = require('@playwright/test')
 // defined in the playwright.config.js file
 
 test.beforeEach(async ({ request }) => {
-  request.post('/reset', { data: { todos: [] } })
+  await request.post('/reset', { data: { todos: [] } })
 })
 
 test('has title', async ({ page }, testInfo) => {

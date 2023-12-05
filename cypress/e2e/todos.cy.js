9b88892dd122ab1b7931791def2a22ff52974314
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-12-05 18:04:20 -0500

    d2 initial code

--- cypress/e2e/todos.cy.js
+++ cypress/e2e/todos.cy.js
@@ -3,7 +3,8 @@
 
 describe('App', () => {
   beforeEach(() => {
-    cy.intercept('GET', '/todos', { body: [] })
+    // stub the "GET /todos" network calls
+    // and return an empty array
     cy.visit('/')
   })
 
@@ -11,35 +12,24 @@ describe('App', () => {
     const todos = '.todo-list li'
     // confirm the application has finished loading
     // by checking the presence of an element with class "loaded"
-    cy.get('.loaded')
     // and there are no items
-    cy.get(todos).should('have.length', 0)
+
     // spy on the "POST /todos" call
     // give the spy an alias "post-todo"
     // https://on.cypress.io/intercept
     // https://on.cypress.io/as
-    cy.intercept('POST', '/todos').as('post-todo')
     // type new todo "Learn testing" followed by Enter
-    cy.get('.new-todo').type('Learn testing{enter}')
+
     // confirm the new todo was sent over the network
-    cy.wait('@post-todo')
-      // get the request body and confirm the known properties
-      // like "title" and "completed"
-      // Tip: use the Chai assertion "deep.include"
-      // https://glebbahmutov.com/cypress-examples/commands/assertions.html
-      .its('request.body')
-      .should('deep.include', {
-        title: 'Learn testing',
-        completed: false,
-      })
-      // confirm the request body includes the property "id"
-      // which should be a string
-      .its('id')
-      .should('be.a', 'string')
+    // get the request body and confirm the known properties
+    // like "title" and "completed"
+    // Tip: use the Chai assertion "deep.include"
+    // https://glebbahmutov.com/cypress-examples/commands/assertions.html
+    // confirm the request body includes the property "id"
+    // which should be a string
     // get the network intercept with the new todo again
     // and confirm the server responds with status code 201
     // Tip: you wait for the network intercept to happen
     // and then can use cy.get('@alias') to retrieve it multiple times
-    cy.get('@post-todo').its('response.statusCode').should('equal', 201)
   })
 })
--- pw/todos.spec.js
+++ pw/todos.spec.js
@@ -3,44 +3,21 @@ const { test, expect } = require('@playwright/test')
 
 test.describe('App', () => {
   test.beforeEach(async ({ page }) => {
-    await page.route('/todos', (route) => {
-      if (route.request().method() === 'GET') {
-        return route.fulfill({
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify([]),
-        })
-      } else {
-        return route.continue()
-      }
-    })
+    // intercept the route "/todos"
+    // - "GET /todos" respond with an empty list
+    // - otherwise let the request continue
     await page.goto('/')
   })
 
   test('shows the items with css class', async ({ page }) => {
     // confirm the application has finished loading
     // by checking the presence of an element with class "loaded"
-    await page.locator('.loaded').waitFor()
     // there should be no todos
-    await expect(page.locator('.todo-list li')).toHaveCount(0)
     // spy on the "POST /todos" call
-    const postTodo = page.waitForRequest((req) => {
-      return req.method() === 'POST' && req.url().endsWith('/todos')
-    })
     // type new todo "Learn testing" followed by Enter
-    await page.locator('.new-todo').fill('Learn testing')
-    await page.locator('.new-todo').press('Enter')
     // confirm the new todo was sent over the network
-    const request = await postTodo
     // get the request data and confirm the known properties
     // like "title" and "completed"
     // the "id" property should be a string
-    const sent = JSON.parse(request.postData() || '')
-    expect(sent, 'request data').toEqual({
-      title: 'Learn testing',
-      completed: false,
-      id: expect.any(String),
-    })
-    const response = await request.response()
-    expect(response?.status(), 'status code').toEqual(201)
   })
 })

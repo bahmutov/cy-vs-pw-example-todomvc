74b196207c0cc1d2290cd36490b7158dba5ab96c
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-12-06 11:21:05 -0500

    d3 solution

--- cypress/e2e/todos.cy.js
+++ cypress/e2e/todos.cy.js
@@ -3,43 +3,39 @@
 
 describe('App', () => {
   beforeEach(() => {
-    cy.intercept('GET', '/todos', { body: [] })
+    // disable network caching using a Chrome Debugger Protocol command
+    // by using "cy.wrap" command we ensure that the promise returned
+    // by the Cypress.automation method resolves before proceeding
+    // to the next Cypress command
+    cy.wrap(
+      Cypress.automation('remote:debugger:protocol', {
+        command: 'Network.setCacheDisabled',
+        params: {
+          cacheDisabled: true,
+        },
+      }),
+    )
+    // spy on the network call "GET /todos"
+    // give the network intercept an alias
+    cy.intercept('GET', '/todos').as('load')
     cy.visit('/')
   })
 
-  it('sends new todo object', () => {
-    const todos = '.todo-list li'
-    // confirm the application has finished loading
-    // by checking the presence of an element with class "loaded"
-    cy.get('.loaded')
-    // and there are no items
-    cy.get(todos).should('have.length', 0)
-    // spy on the "POST /todos" call
-    // give the spy an alias "post-todo"
-    // https://on.cypress.io/intercept
-    // https://on.cypress.io/as
-    cy.intercept('POST', '/todos').as('post-todo')
-    // type new todo "Learn testing" followed by Enter
-    cy.get('.new-todo').type('Learn testing{enter}')
-    // confirm the new todo was sent over the network
-    cy.wait('@post-todo')
-      // get the request body and confirm the known properties
-      // like "title" and "completed"
-      // Tip: use the Chai assertion "deep.include"
-      // https://glebbahmutov.com/cypress-examples/commands/assertions.html
-      .its('request.body')
-      .should('deep.include', {
-        title: 'Learn testing',
-        completed: false,
+  it('uses shows the same number of items as sent by the server', () => {
+    // wait for the network alias
+    // get its response body and confirm
+    // it is an array
+    // grab its length and pass it to the cy.then callback
+    // inside the callback get the number of Todo items
+    // on the page, it should equal to the number of items
+    // returned by the server
+    // https://on.cypress.io/then
+    cy.wait('@load')
+      .its('response.body')
+      .should('be.an', 'array')
+      .its('length')
+      .then((n) => {
+        cy.get('.todo-list li').should('have.length', n)
       })
-      // confirm the request body includes the property "id"
-      // which should be a string
-      .its('id')
-      .should('be.a', 'string')
-    // get the network intercept with the new todo again
-    // and confirm the server responds with status code 201
-    // Tip: you wait for the network intercept to happen
-    // and then can use cy.get('@alias') to retrieve it multiple times
-    cy.get('@post-todo').its('response.statusCode').should('equal', 201)
   })
 })
--- pw/todos.spec.js
+++ pw/todos.spec.js
@@ -2,45 +2,24 @@
 const { test, expect } = require('@playwright/test')
 
 test.describe('App', () => {
+  let load
+
   test.beforeEach(async ({ page }) => {
-    await page.route('/todos', (route) => {
-      if (route.request().method() === 'GET') {
-        return route.fulfill({
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify([]),
-        })
-      } else {
-        return route.continue()
-      }
-    })
+    // spy on the network calls to "/todos" endpoint
+    load = page.waitForRequest('/todos')
     await page.goto('/')
   })
 
-  test('shows the items with css class', async ({ page }) => {
-    // confirm the application has finished loading
-    // by checking the presence of an element with class "loaded"
-    await page.locator('.loaded').waitFor()
-    // there should be no todos
-    await expect(page.locator('.todo-list li')).toHaveCount(0)
-    // spy on the "POST /todos" call
-    const postTodo = page.waitForRequest((req) => {
-      return req.method() === 'POST' && req.url().endsWith('/todos')
-    })
-    // type new todo "Learn testing" followed by Enter
-    await page.locator('.new-todo').fill('Learn testing')
-    await page.locator('.new-todo').press('Enter')
-    // confirm the new todo was sent over the network
-    const request = await postTodo
-    // get the request data and confirm the known properties
-    // like "title" and "completed"
-    // the "id" property should be a string
-    const sent = JSON.parse(request.postData() || '')
-    expect(sent, 'request data').toEqual({
-      title: 'Learn testing',
-      completed: false,
-      id: expect.any(String),
-    })
+  test('uses shows the same number of items as sent by the server', async ({
+    page,
+  }) => {
+    // confirm the network call has happened
+    // and get the response as json
+    const request = await load
     const response = await request.response()
-    expect(response?.status(), 'status code').toEqual(201)
+    const todos = await response?.json()
+    // confirm the page shows the same number of todo items
+    // as send by the server
+    await expect(page.locator('.todo-list li')).toHaveCount(todos.length)
   })
 })

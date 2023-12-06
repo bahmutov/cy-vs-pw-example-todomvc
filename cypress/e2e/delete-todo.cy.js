65c47b50f26f161e37e5521e5a7fb7dcf2ef416a
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-12-06 16:59:51 -0500

    initial code for d4

--- cypress/e2e/delete-todo.cy.js
+++ cypress/e2e/delete-todo.cy.js
@@ -12,24 +12,13 @@ describe('App', () => {
     const title = 'Advance ' + Cypress._.random(1e6)
 
     // spy on the "POST /todos" network call
-    cy.intercept('POST', '/todos').as('post')
     // enter the new todo with a random title
-    cy.get('input.new-todo').type(title + '{enter}')
     // wait for the post call and get the todo item id
-    cy.wait('@post')
-      .its('request.body.id')
-      .should('be.a', 'string')
-      .then((id) => {
-        // spy on the "DELETE /todos/:id" network call
-        cy.intercept('DELETE', `/todos/${id}`).as('del')
-        // find the newly entered todo item
-        // and click on the delete button
-        cy.contains('li.todo', title).find('.destroy').click({ force: true })
-        // confirm the delete network call happens
-        // and the server responded with the status code 200
-        cy.wait('@del').its('response.statusCode').should('equal', 200)
-      })
+    // spy on the "DELETE /todos/:id" network call
+    // find the newly entered todo item
+    // and click on the delete button
+    // confirm the delete network call happens
+    // and the server responded with the status code 200
     // confirm the new todo is no longer on the page
-    cy.contains('li.todo', title).should('not.exist')
   })
 })
--- pw/delete-todo.spec.js
+++ pw/delete-todo.spec.js
@@ -2,10 +2,9 @@
 const { test, expect } = require('@playwright/test')
 
 test.describe('App', () => {
-  let load
-
   test.beforeEach(async ({ page }) => {
     await page.goto('/')
+    // wait for the page to load the todos
     await page.locator('.loaded').waitFor()
   })
 
@@ -13,30 +12,13 @@ test.describe('App', () => {
     const title = 'Advance ' + Math.round(Math.random() * 1e6)
 
     // spy on the "POST /todos" network call
-    const post = page.waitForRequest(
-      (req) => req.method() === 'POST' && req.url().endsWith('/todos'),
-    )
     // enter the new todo with a random title
-    await page.locator('input.new-todo').fill(title)
-    await page.locator('input.new-todo').press('Enter')
     // wait for the post call and get the todo item id
-    const request = await post
-    const id = (await (await request.response())?.json())?.id
     // spy on the "DELETE /todos/:id" network call
-    const del = page.waitForRequest(
-      (req) => req.method() === 'DELETE' && req.url().endsWith(`/todos/${id}`),
-    )
     // find the newly entered todo item
     // and click on the delete button
-    const todoElement = await page.locator('li.todo', { hasText: title })
-    await todoElement.hover()
-    await todoElement.locator('.destroy').click()
     // confirm the delete network call happens
     // and the server responded with the status code 200
-    const delRequest = await del
-    const statusCode = (await delRequest.response())?.status()
-    expect(statusCode, 'delete status code').toEqual(200)
     // confirm the new todo is no longer on the page
-    await expect(todoElement).not.toBeAttached()
   })
 })

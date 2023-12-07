dd1cf5b74d537ef7734d5c8ecac7c2c64a89a176
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-12-06 20:37:08 -0500

    initial code for d5

--- cypress/e2e/loader.cy.js
+++ cypress/e2e/loader.cy.js
@@ -7,14 +7,11 @@ describe('App', () => {
     // let the call continue to the server
     // but delay it by 2 seconds
     // This should give the loading element plenty of time
-    cy.intercept('GET', '/todos', () => Cypress.Promise.delay(2000))
     cy.visit('/')
     // confirm the loading element is visible
     // and then becomes hidden
-    cy.get('.loading').should('be.visible')
-    cy.get('.loading').should('not.be.visible')
+
     // confirm the app finishes loading really quickly
     // after the loader becomes hidden
-    cy.get('.loaded', { timeout: 100 })
   })
 })

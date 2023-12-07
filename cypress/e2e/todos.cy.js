d6db1446926505b61e4bb44ea84fbb6ea15f7421
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-12-07 18:32:55 -0500

    e1 solution using Cypress

--- cypress/e2e/todos.cy.js
+++ cypress/e2e/todos.cy.js
@@ -1,58 +1,60 @@
 // @ts-check
 /// <reference types="cypress" />
 
+chai.config.truncateThreshold = 400
+
 describe('App', () => {
-  it('stubs the load data network call three different ways', () => {
-    const todos = '.todo-list li'
-
-    // stub the "GET /todos" network call
-    // on the first call return the data from the "fixtures/one.json" file
-    // on the second call return the data from the "fixtures/two.json" file
-    // on the third call return the data from the "fixtures/three.json" file
-    // Tip: while you can write a callback that keeps the request count
-    // to decide which fixture to return, you can also define a Cypress intercept
-    // to work only a certain number of times
-    // See the https://on.cypress.io/intercept options
-    cy.intercept(
-      {
-        method: 'GET',
-        path: '/todos',
-      },
-      { fixture: 'three.json' },
-    )
-
-    cy.intercept(
-      {
-        method: 'GET',
-        path: '/todos',
-        times: 1,
-      },
-      { fixture: 'two.json' },
-    )
-
-    cy.intercept(
-      {
-        method: 'GET',
-        path: '/todos',
-        times: 1,
-      },
-      { fixture: 'one.json' },
-    )
-    // load the page
-    // and confirm only 1 todo is shown
+  beforeEach(() => {
+    cy.request('POST', '/reset', { todos: [] })
     cy.visit('/')
-    cy.get(todos).should('have.length', 1)
-    // reload the page
-    // confirm there are 2 todos
-    cy.reload()
-    cy.get(todos).should('have.length', 2)
-    // reload the page
-    // confirm there are 3 todos
-    cy.reload()
-    cy.get(todos).should('have.length', 3)
-    // reload the page one more time
-    // and confirm the 3 todos are still there
-    cy.reload()
-    cy.get(todos).should('have.length', 3)
+    cy.get('.loaded')
+  })
+
+  it('confirms the entire sent Todo object', () => {
+    // spy on the "POST /todos" network call
+    // and give it an alias "post"
+    // https://on.cypress.io/intercept
+    // https://on.cypress.io/as
+    cy.intercept('POST', '/todos').as('post')
+    // get the application's window object
+    // and spy on its "Math.random" method
+    // https://on.cypress.io/window
+    // https://on.cypress.io/its
+    // https://on.cypress.io/spy
+    // give the method spy an alias "random"
+    cy.window()
+      .its('Math')
+      .then((Math) => {
+        cy.spy(Math, 'random').as('random')
+      })
+    // enter new todo "Code" followed by enter
+    cy.get('input.new-todo').type('Code{enter}')
+    // get the "random" spy
+    // https://on.cypress.io/get
+    // and confirm it was called once
+    // https://glebbahmutov.com/cypress-examples/commands/spies-stubs-clocks.html
+    cy.get('@random')
+      .should('have.been.calledOnce')
+      // from the spy, get the first call's return value
+      // using "firstCall.returnValue" deep notation
+      // https://on.cypress.io/its
+      .its('firstCall.returnValue')
+      // and confirm it is a number between 0 and 1
+      .should('be.within', 0, 1)
+      // follow the same steps as the app.js does to
+      // convert the float to the string id
+      .invoke('toString')
+      .invoke('substr', 2, 10)
+      // pass the id value into cy.then callback and
+      // get the "post" network call post data
+      // you know know all the fields it should have
+      // confirm using the "deep.equal" assertion
+      .then((id) => {
+        cy.wait('@post').its('request.body').should('deep.equal', {
+          title: 'Code',
+          completed: false,
+          id,
+        })
+      })
   })
 })

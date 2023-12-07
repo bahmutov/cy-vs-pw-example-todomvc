3a3ec0e7fa9328fa6407db5b003a70e17d515b99
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-12-07 09:14:23 -0500

    d7 initial code

--- pw/same-response.spec.js
+++ pw/same-response.spec.js
@@ -6,18 +6,9 @@ test.describe('App', () => {
     await page.goto('/')
     await page.locator('.loaded').waitFor()
     // spy on the "POST /todos" call
-    const post = page.waitForRequest(
-      (req) => req.method() === 'POST' && req.url().endsWith('/todos'),
-    )
     // enter a new todo "Test"
-    await page.locator('input.new-todo').fill('Test')
-    await page.locator('input.new-todo').press('Enter')
     // wait for the POST call to happen
-    const request = await post
     // get the send data and the response data
-    const sendData = request.postDataJSON()
-    const receivedData = await (await request.response())?.json()
     // and confirm they are the same
-    expect(sendData, 'same item').toEqual(receivedData)
   })
 })

fec61e446171f6c77be20bc9538cb3cd99769d3f
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-12-07 12:54:24 -0500

    initial code for d9

--- cypress/e2e/todos.cy.js
+++ cypress/e2e/todos.cy.js
@@ -11,26 +11,14 @@ describe('App', () => {
   it('assigns a different id to each new item', () => {
     // start spying on the "POST /todos" calls
     // assign the intercept an alias "post-todo"
-    cy.intercept('POST', '/todos').as('post-todo')
     // add new todo with text "first todo"
-    cy.get('.new-todo').type('first todo{enter}')
     // get the request id sent by the application
     // from the network call "post-todo"
     // confirm it is a string
-    cy.wait('@post-todo')
-      .its('request.body.id')
-      .should('be.a', 'string')
-      .then((id) => {
-        // add new todo with text "second todo"
-        cy.get('.new-todo').type('second todo{enter}')
-        // get the request id from the second todo
-        // sent by the application
-        // confirm it is a string
-        // and it is different from the first request
-        cy.wait('@post-todo')
-          .its('request.body.id')
-          .should('be.a', 'string')
-          .and('not.equal', id)
-      })
+    // add new todo with text "second todo"
+    // get the request id from the second todo
+    // sent by the application
+    // confirm it is a string
+    // and it is different from the first request
   })
 })
--- pw/todos.spec.js
+++ pw/todos.spec.js
@@ -10,30 +10,15 @@ test.describe('App', () => {
 
   test('assigns a different id to each new item', async ({ page }) => {
     // spy on the first call to "POST /todos"
-    const routeMatcher = (req) => {
-      return req.method() === 'POST' && req.url().endsWith('/todos')
-    }
-    const postTodo1 = page.waitForRequest(routeMatcher)
     // add new todo with text "first todo"
-    await page.locator('.new-todo').fill('first todo')
-    await page.locator('.new-todo').press('Enter')
     // get the request id sent by the application
     // from the network call "post-todo"
     // confirm it is a string
-    const id1 = (await postTodo1).postDataJSON().id
-    expect(id1, 'first id').toEqual(expect.any(String))
-
     // spy on the second call to "POST /todos"
-    const postTodo2 = page.waitForRequest(routeMatcher)
     // add new todo with text "second todo"
-    await page.locator('.new-todo').fill('first todo')
-    await page.locator('.new-todo').press('Enter')
     // get the request id from the second todo
     // sent by the application
     // confirm it is a string
-    const id2 = (await postTodo2).postDataJSON().id
-    expect(id2, 'second id').toEqual(expect.any(String))
     // and it is different from the first request
-    expect(id2, 'ids are different').not.toEqual(id1)
   })
 })

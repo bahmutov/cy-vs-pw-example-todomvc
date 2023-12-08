5490dd9cbe5d4d746bf7f52125bd2eee0bdc4de1
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-12-08 14:20:04 -0500

    add d11 initial code

--- cypress/e2e/todos.cy.js
+++ cypress/e2e/todos.cy.js
@@ -5,6 +5,7 @@ describe('App', () => {
   it('inserts the first todo', () => {
     const todos = '.todo-list li'
     const title = 'The first one!'
+
     // spy on the "GET /todos" network call
     // before the request continues
     // delete the header "if-none-match" to avoid
@@ -14,17 +15,6 @@ describe('App', () => {
     // and insert a new object at the first position
     // title, completed=false, id="1234"
     // https://on.cypress.io/intercept
-    cy.intercept('GET', '/todos', (req) => {
-      delete req.headers['if-none-match']
-      req.continue((res) => {
-        expect(res.body, 'response list').to.be.an('array')
-        res.body.unshift({
-          title,
-          completed: false,
-          id: '1234',
-        })
-      })
-    })
 
     cy.visit('/')
     // confirm there is at least one todo
@@ -32,11 +22,5 @@ describe('App', () => {
     // has the title text
     // and has the class "todo"
     // and does not have class "completed"
-    cy.get(todos)
-      .should('have.length.greaterThan', 0)
-      .first()
-      .should('include.text', title)
-      .and('have.class', 'todo')
-      .and('not.have.class', 'completed')
   })
 })
--- pw/todos.spec.js
+++ pw/todos.spec.js
@@ -4,22 +4,14 @@ const { test, expect } = require('@playwright/test')
 test.describe('App', () => {
   test('inserts the first todo', async ({ page }) => {
     const todos = page.locator('.todo-list li')
-
     const title = 'The first one!'
+
     // spy on the "GET /todos" network call
     // once the response arrives
     // confirm it is an array
     // and insert a new object at the first position
     // title, completed=false, id="1234"
     // https://playwright.dev/docs/mock
-    await page.route('/todos', async (route) => {
-      const response = await route.fetch()
-      const json = await response.json()
-      json.unshift({ title, completed: false, id: '1234' })
-      // Fulfill using the original response, while patching the response body
-      // with the given JSON object.
-      await route.fulfill({ response, json })
-    })
 
     await page.goto('/')
 
@@ -28,12 +20,5 @@ test.describe('App', () => {
     // has the title text
     // and has the class "todo"
     // and does not have class "completed"
-    await expect(async () => {
-      const n = await todos.count()
-      expect(n).toBeGreaterThan(0)
-    }).toPass()
-    await expect(todos.first()).toHaveText(title)
-    await expect(todos.first()).toHaveClass(/todo/)
-    await expect(todos.first()).not.toHaveClass(/completed/)
   })
 })

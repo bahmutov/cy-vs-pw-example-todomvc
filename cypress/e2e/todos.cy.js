7ae21396194dba84cf43243cb962fef73a7c80c6
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-12-08 11:33:06 -0500

    comments

--- cypress/e2e/todos.cy.js
+++ cypress/e2e/todos.cy.js
@@ -15,46 +15,31 @@ describe('App', () => {
     // and give it an alias "post"
     // https://on.cypress.io/intercept
     // https://on.cypress.io/as
-    cy.intercept('POST', '/todos').as('post')
+
     // get the application's window object
     // and spy on its "Math.random" method
     // https://on.cypress.io/window
     // https://on.cypress.io/its
     // https://on.cypress.io/spy
     // give the method spy an alias "random"
-    cy.window()
-      .its('Math')
-      .then((Math) => {
-        cy.spy(Math, 'random').as('random')
-      })
+
     // enter new todo "Code" followed by enter
     cy.get('input.new-todo').type('Code{enter}')
     // get the "random" spy
     // https://on.cypress.io/get
     // and confirm it was called once
     // https://glebbahmutov.com/cypress-examples/commands/spies-stubs-clocks.html
-    cy.get('@random')
-      .should('have.been.calledOnce')
-      // from the spy, get the first call's return value
-      // using "firstCall.returnValue" deep notation
-      // https://on.cypress.io/its
-      .its('firstCall.returnValue')
-      // and confirm it is a number between 0 and 1
-      .should('be.within', 0, 1)
-      // follow the same steps as the app.js does to
-      // convert the float to the string id
-      .invoke('toString')
-      .invoke('substr', 2, 10)
-      // pass the id value into cy.then callback and
-      // get the "post" network call post data
-      // you know know all the fields it should have
-      // confirm using the "deep.equal" assertion
-      .then((id) => {
-        cy.wait('@post').its('request.body').should('deep.equal', {
-          title: 'Code',
-          completed: false,
-          id,
-        })
-      })
+
+    // from the spy, get the first call's return value
+    // using "firstCall.returnValue" deep notation
+    // https://on.cypress.io/its
+    // and confirm it is a number between 0 and 1
+    // follow the same steps as the app.js does to
+    // convert the float to the string id
+
+    // pass the id value into cy.then callback and
+    // get the "post" network call post data
+    // you know know all the fields it should have
+    // confirm using the "deep.equal" assertion
   })
 })
--- pw/todos.spec.js
+++ pw/todos.spec.js
@@ -10,7 +10,7 @@ test.describe('App', () => {
 
   test('confirms the entire sent Todo object', async ({ page }) => {
     // start spying on the network requests to "/todos"
-    const post = page.waitForRequest('/todos')
+
     // https://playwright.dev/docs/api/class-page
     // using the "page.evaluate" method
     // inject into the application's window a script
@@ -21,15 +21,7 @@ test.describe('App', () => {
     // - returns the created random float to the caller
     // Tip: to later be able to read the generated float
     // stores it as a property of the window object
-    page.evaluate(() => {
-      const rand = Math.random
-      Math.random = () => {
-        // @ts-ignore
-        window.lastRandom = rand()
-        // @ts-ignore
-        return window.lastRandom
-      }
-    })
+
     // type new type "Code" into the input box
     await page.locator('input.new-todo').fill('Code')
     await page.locator('input.new-todo').press('Enter')
@@ -37,21 +29,10 @@ test.describe('App', () => {
     // use the "page.evaluate" to get the saved valued
     // which is the random number the app used to derive the todo "id"
     // use the same logic to form the string item ID value
-    const id = await page.evaluate(() =>
-      // @ts-ignore
-      String(window.lastRandom).substr(2, 10),
-    )
+
     // log the id the app received
-    console.log(`new item id ${id}`)
     // wait for the network call to post the new todo
-    const request = await post
     // get the post network request data
-    const data = request.postDataJSON()
     // and confirm ALL its fields
-    expect(data, 'posted').toEqual({
-      title: 'Code',
-      completed: false,
-      id,
-    })
   })
 })

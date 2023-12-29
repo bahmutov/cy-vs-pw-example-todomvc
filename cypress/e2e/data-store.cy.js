cf29efb46f08c65cc7c68d0db3dcbe94934ce1a0
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-12-29 13:41:50 -0500

    initial code

--- cypress/e2e/data-store.cy.js
+++ cypress/e2e/data-store.cy.js
@@ -9,24 +9,14 @@ describe('App', () => {
       cy.request('POST', '/reset', { todos })
     })
     // signal to the app to set the "window.app" object
-    cy.visit('/', {
-      onBeforeLoad(win) {
-        // @ts-ignore
-        win.exposeAppInstanceDuringTests = true
-      },
-    })
+    cy.visit('/')
     // there should be two items on the page
-    cy.get('.todo').should('have.length', 2)
+
     // there should be an object "app" on the "window" of the application
-    cy.window().should('have.property', 'app')
+
     // the "app" object should have a property "todos"
     // with 2 items. If you map each item to its "title" property
     // it should yield an array with:
     // ["Code the app", "Write the tests"]
-    cy.window()
-      .its('app.todos')
-      .should('have.length', 2)
-      .invoke('map', (t) => t.title)
-      .should('deep.equal', ['Code the app', 'Write the tests'])
   })
 })

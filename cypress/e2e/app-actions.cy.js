3eab9204623fbf4202773ca39be7d013d16a9a1d
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-01-10 21:58:45 -0500

    initial code

--- cypress/e2e/app-actions.cy.js
+++ cypress/e2e/app-actions.cy.js
@@ -9,15 +9,13 @@ describe('App', () => {
       cy.request('POST', '/reset', { todos })
     })
     cy.visit('/')
-      // confirm the window object has the "app" property
-      .should('have.property', 'app')
+    // confirm the window object has the "app" property
     // there should be two items on the page
-    cy.get('.todo').should('have.length', 2)
+    //
     // call the method "dispatch" on the "app.$store" object
     // to add a new todo with the text "Run tests"
-    cy.window().its('app.$store').invoke('dispatch', 'addTodo', 'Run tests')
+    //
     // confirm the list of todos has text
     // "Run tests" as the last item
-    cy.get('li.todo').last().find('label').should('have.text', 'Run tests')
   })
 })
--- pw/app-actions.spec.js
+++ pw/app-actions.spec.js
@@ -12,22 +12,12 @@ test.describe('App', () => {
     // visit the home page
     await page.goto('/')
     // there should be two items on the page
-    await expect(page.locator('.todo')).toHaveCount(2)
     // there should be an object "app" on the "window" of the application
-    const appPresent = await page.evaluate(() => {
-      return 'app' in window
-    })
-    expect(appPresent, 'window.app is present').toBeTruthy()
+    //
     // call the method "dispatch" on the "app.$store" object
     // to add a new todo with the text "Run tests"
-    await page.evaluate(() => {
-      // @ts-ignore
-      window.app.$store.dispatch('addTodo', 'Run tests')
-    })
+    //
     // confirm the list of todos has text
     // "Run tests" as the last item
-    await expect(page.locator('li.todo').last().locator('label')).toHaveText(
-      'Run tests',
-    )
   })
 })

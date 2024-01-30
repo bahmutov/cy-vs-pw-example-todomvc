1520334f069c948bea1a0d4adfe1d203cc992758
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-01-30 11:01:33 -0500

    example

--- cypress/e2e/multiple-tests.cy.js
+++ cypress/e2e/multiple-tests.cy.js
@@ -1,33 +1,20 @@
 // @ts-check
 /// <reference types="cypress" />
 
-import todos from '../../fixtures/two-items.json'
+import 'cypress-map'
 
-// how would you ensure that these tests run one after another
-// and do not go to the blank page in between?
-describe('Completing todos', () => {
-  before(() => {
-    // confirm there are more than 1 todos
-    expect(todos).to.have.length.greaterThan(1)
-    cy.request('POST', '/reset', { todos })
-  })
+import todos from '../../fixtures/three-items.json'
 
-  it('shows the list of todos', () => {
-    cy.visit('/')
-    // confirm the number of todos shown
-    cy.log('**all todos are active**')
-    // confirm the all todos are active (do not have class completed)
-    // there should be no completed todos
-  })
+before(() => {
+  // confirm there are more than 1 todos
+  expect(todos).to.have.length.greaterThan(1)
+  cy.request('POST', '/reset', { todos })
+})
 
-  it('completes the first todo', () => {
-    // find the first todo and click on its ".toggle" button
-    // the first todo should get the class "completed"
-  })
-
-  it('clears completed todos', () => {
-    // click on the button with the text "Clear completed"
-    // the number of todos should go down by 1
-    // there should be no completed todos
-  })
+it('shows the list of todos', () => {
+  cy.visit('/')
+  cy.get('li.todo').should('have.length', todos.length)
+  cy.get('li.todo').first().should('contain', todos[0].title)
+  cy.get('li.todo').second().should('contain', todos[1].title)
+  cy.get('li.todo').third().should('contain', todos[2].title)
 })

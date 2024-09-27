336dca6579276a9a29645865c2dcba16a2aae0ba
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-09-26 22:40:53 -0400

    finished

--- pw/clock.spec.js
+++ pw/clock.spec.js
@@ -3,26 +3,19 @@ const { test, expect } = require('@playwright/test')
 
 test.describe('App', () => {
   test('fetches todos every 60 seconds', async ({ page }) => {
+    // https://playwright.dev/docs/clock
     await page.clock.install()
 
-    // spy on the "/todos" network request
     const loadTodos1 = page.waitForRequest('/todos')
     await page.goto('/')
-    // confirm the application fetches the "/todos" endpoint
     await loadTodos1
 
-    // spy on the "/todos" network request again
     const loadTodos2 = page.waitForRequest('/todos')
-    // advance the fake timers by 61 seconds
     await page.clock.runFor(61_000)
-    // and confirm the application fetches the "/todos" endpoint again
     await loadTodos2
 
-    // spy on the "/todos" network request again
     const loadTodos3 = page.waitForRequest('/todos')
-    // advance the fake timers by 61 seconds
     await page.clock.runFor(61_000)
-    // and confirm the application fetches the "/todos" endpoint 3rd time
     await loadTodos3
   })
 })

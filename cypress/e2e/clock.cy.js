815a3e106cc9e27aa679c81d8cd84613b99f941a
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-09-26 22:35:26 -0400

    initial

--- cypress/e2e/clock.cy.js
+++ cypress/e2e/clock.cy.js
@@ -9,19 +9,15 @@ describe('App', () => {
     cy.intercept('GET', '/todos').as('loadTodos')
     // control the application's clock
     // https://on.cypress.io/clock
-    cy.clock()
     cy.visit('/')
     // confirm the application called the "loadTodos" endpoint
     // Tip: you can even use a short 100ms timeout
     cy.wait('@loadTodos', { timeout: 100 })
     // advance the application's clock by 61 seconds
     // https://on.cypress.io/tick
-    cy.tick(61_000)
     // confirm the application called the "loadTodos" endpoint
-    cy.wait('@loadTodos', { timeout: 100 })
+
     // advance the application's clock by another 61 seconds
-    cy.tick(61_000)
     // confirm the application called the "loadTodos" endpoint
-    cy.wait('@loadTodos', { timeout: 100 })
   })
 })

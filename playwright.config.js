3cfb1c4862f1e0cace609406444e3454439016f8
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2025-11-12 06:42:28 +0900

    prepapre

--- playwright.config.js
+++ playwright.config.js
@@ -33,9 +33,36 @@ module.exports = defineConfig({
 
   /* Configure projects for major browsers */
   projects: [
+    /* Test against desktop browsers */
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+    /* Test against mobile viewports. */
+    {
+      name: 'Mobile Chrome',
+      use: { ...devices['Pixel 5'] },
+    },
+    {
+      name: 'Mobile Safari',
+      use: { ...devices['iPhone 12'] },
+    },
+    /* Test against branded browsers. */
+    {
+      name: 'Google Chrome',
+      use: { ...devices['Desktop Chrome'], channel: 'chrome' }, // or 'chrome-beta'
+    },
+    {
+      name: 'Microsoft Edge',
+      use: { ...devices['Desktop Edge'], channel: 'msedge' }, // or 'msedge-dev'
     },
   ],
 })

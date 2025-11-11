60f08a3f71b9248e2245d1e06dd579004d494848
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2025-11-12 06:49:15 +0900

    initial specs

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -4,16 +4,6 @@
 it('has title', () => {
   // visit the page "localhost:3000"
   // https://on.cypress.io/visit
-  cy.visit('http://localhost:3000/')
-
   // the page title should have text "cy-vs-pw-example-todomvc"
   // https://on.cypress.io/title
-  cy.title().should('equal', 'cy-vs-pw-example-todomvc')
-
-  // confirm there are 3 todo items on the page
-  // use the CSS selector ".todo-list li"
-  // https://on.cypress.io/get
-  // and "should have length" assertion
-  // https://on.cypress.io/should
-  cy.get('.todo-list li').should('have.length', 3)
 })
--- pw/example.spec.js
+++ pw/example.spec.js
@@ -2,7 +2,6 @@
 const { test, expect } = require('@playwright/test')
 
 test('has title', async ({ page }) => {
-  await page.goto('http://localhost:3000/')
-
+  // visit the page "localhost:3000"
   // Expect a title "to contain" a substring.
 })

20e711d723b847e11a73933cd5ab6e23f115bc5f
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2025-12-04 10:06:21 -0500

    log hello

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,27 +1,6 @@
 // @ts-check
 /// <reference types="cypress" />
 
-// commands operate with respect to the "baseUrl"
-// defined in the cypress.config.js file
-
-// beforeEach(() => {
-//   cy.request('POST', '/reset', { todos: [] })
-// })
-
-// Tip: look at the following commands before writing this test
-// https://on.cypress.io/visit
-// https://on.cypress.io/get
-// https://on.cypress.io/type
-it('has title', () => {
-  cy.visit('http://localhost:3000')
-  cy.get('body.loaded').should('be.visible')
-  cy.get('.todo-list li').should('have.length', 0)
-
-  // find the input element using the placeholder text
-  // and type "Write code" followed by "Enter" press
-  cy.get('[placeholder="What needs to be done?"]').type('Write code{enter}')
-
-  cy.get('.todo-list li label')
-    .should('have.length', 1)
-    .and('have.text', 'Write code')
+it('prints hello', () => {
+  console.log('hello')
 })
--- pw/example.spec.js
+++ pw/example.spec.js
@@ -1,26 +1,6 @@
 // @ts-check
-const { test, expect } = require('@playwright/test')
+const { test } = require('@playwright/test')
 
-// start each test with zero todos
-// test.beforeEach(async ({ request }) => {
-//   request.post('/reset', { data: { todos: [] } })
-// })
-
-// Tip: read the "Actions" Guide before implementing this test
-// https://playwright.dev/docs/input
-test('adding todos', async ({ page }) => {
-  // visit the application
-  await page.goto('http://localhost:3000')
-  // wait for the body.loaded element to be visible
-  await expect(page.locator('body.loaded')).toBeVisible()
-  // there should be zero todo items
-  await expect(page.locator('.todo-list li')).toHaveCount(0)
-  // find the input element using the placeholder text
-  // and type "Write code" followed by "Enter" press
-  await page.getByPlaceholder('What needs to be done?').fill('Write code')
-  await page.getByPlaceholder('What needs to be done?').press('Enter')
-  // there should be one todo item on the page
-  await expect(page.locator('.todo-list li')).toHaveCount(1)
-  // the todo item label should have the entered text
-  await expect(page.locator('.todo-list li label')).toHaveText('Write code')
+test('prints hello', async () => {
+  console.log('hello')
 })

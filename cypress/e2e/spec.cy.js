3ee713bae824127db07f42710b701ed930205760
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2025-12-06 14:23:50 -0500

    use wrong span

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -8,23 +8,17 @@ beforeEach(() => {
   cy.request('POST', '/reset', { todos: [] })
 })
 
-// Tip: look at the following commands before writing this test
-// https://on.cypress.io/visit
-// https://on.cypress.io/get
-// https://on.cypress.io/type
-it('has title', () => {
-  // visit the application
+it('adds todos', () => {
   cy.visit('/')
-  // wait for the body.loaded element to be visible
   cy.get('body.loaded').should('be.visible')
-  // there should be zero todo items
-  cy.get('.todo-list li').should('have.length', 0)
-  // find the input element using the placeholder text
-  // and type "Write code" followed by "Enter" press
+
   cy.get('[placeholder="What needs to be done?"]').type('Write code{enter}')
+
   // there should be one todo item on the page
-  // the todo item label should have the entered text
-  cy.get('.todo-list li label')
-    .should('have.length', 1)
-    .and('have.text', 'Write code')
+  cy.get('.todoapp')
+    .find('.todo-list')
+    .find('li.todo')
+    .first()
+    .find('label')
+    .should('have.text', 'Write code')
 })

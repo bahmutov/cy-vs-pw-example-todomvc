18421a8f9c4f47bc8a958102593cb0a3330d0406
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2025-12-06 14:11:55 -0500

    pw

--- pw/example.spec.js
+++ pw/example.spec.js
@@ -3,24 +3,27 @@ const { test, expect } = require('@playwright/test')
 
 // start each test with zero todos
 test.beforeEach(async ({ request }) => {
-  request.post('/reset', { data: { todos: [] } })
+  await request.post('/reset', { data: { todos: [] } })
 })
 
-// Tip: read the "Actions" Guide before implementing this test
-// https://playwright.dev/docs/input
 test('adding todos', async ({ page }) => {
-  // visit the application
   await page.goto('/')
-  // wait for the body.loaded element to be visible
+  // use CSS selector as a locator
   await expect(page.locator('body.loaded')).toBeVisible()
-  // there should be zero todo items
-  await expect(page.locator('.todo-list li')).toHaveCount(0)
-  // find the input element using the placeholder text
-  // and type "Write code" followed by "Enter" press
-  await page.getByPlaceholder('What needs to be done?').fill('Write code')
-  await page.getByPlaceholder('What needs to be done?').press('Enter')
+
+  const todoApp = page.locator('.todoapp')
+  const todoInput = todoApp.getByPlaceholder('What needs to be done?')
+  const todoList = todoApp.locator('.todo-list')
+  const todoItems = todoList.locator('li.todo')
+  const firstTodo = todoItems.first()
+  const firstLabel = firstTodo.locator('label')
+
+  await todoInput.fill('Write code')
+  await todoInput.press('Enter')
+
   // there should be one todo item on the page
-  await expect(page.locator('.todo-list li')).toHaveCount(1)
-  // the todo item label should have the entered text
-  await expect(page.locator('.todo-list li label')).toHaveText('Write code')
+  await expect(todoItems).toHaveCount(1)
+  console.log(firstLabel)
+  // with the label "Write code"
+  await expect(firstLabel).toHaveText('Write code')
 })

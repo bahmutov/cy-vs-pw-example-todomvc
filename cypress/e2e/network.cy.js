34bec289e7da8e63d20656b1bdda5af28afdb830
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2025-12-18 15:44:31 -0500

    day 18

--- cypress/e2e/network.cy.js
+++ cypress/e2e/network.cy.js
@@ -2,11 +2,24 @@
 /// <reference types="cypress" />
 
 import todos from '../../fixtures/3-todos.json'
-import 'cypress-map'
 
-it('stub GET /todos call', () => {
+it('stub GET and POST /todos calls', () => {
   cy.intercept('GET', '/todos', { body: todos }).as('getTodos')
+  cy.intercept('POST', '/todos', (req) => {
+    // send the request body back in the response
+    req.reply(req.body)
+  }).as('postTodos')
   cy.visit('/')
   cy.wait('@getTodos')
-  cy.get('.todo-list li label').should('read', Cypress._.map(todos, 'title'))
+  cy.get('.todo-list li').should('have.length', todos.length)
+
+  // enter a new todo item
+  // and confirm the POST /todos request body
+  // is sent correctly
+  const newTodo = 'walk the dog'
+  cy.get('.new-todo').type(`${newTodo}{enter}`)
+  cy.get('.todo-list li').should('have.length', todos.length + 1)
+  cy.wait('@postTodos')
+    .its('request.body')
+    .should('have.property', 'title', newTodo)
 })
--- pw/network.spec.js
+++ pw/network.spec.js
@@ -3,20 +3,47 @@ const { test, expect } = require('@playwright/test')
 
 import todos from '../fixtures/3-todos.json'
 
-test('stub GET /todos call', async ({ page }) => {
-  await page.route('/todos', (route) =>
+test('stub GET and POST /todos calls', async ({ page }) => {
+  await page.route('/todos', (route) => {
+    if (route.request().method() === 'POST') {
+      // send the request body back in the response
+      const postData = route.request().postDataJSON()
+      route.fulfill({
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(postData),
+      })
+      return
+    }
+
     route.fulfill({
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(todos),
-    }),
-  )
+    })
+  })
 
-  const getTodosPromise = page.waitForResponse('**/todos')
-  await page.goto('/')
-  // confirm the application made the GET /todos request
-  await getTodosPromise
-  // confirm the application is showing the stubbed todos
-  await expect(page.locator('.todo-list li')).toHaveText(
-    todos.map((t) => t.title),
+  // now we need to separate waiting for GET vs POST /todos calls
+  const getTodosPromise = page.waitForResponse(
+    (response) =>
+      response.url().endsWith('/todos') &&
+      response.request().method() === 'GET',
   )
+  await page.goto('/')
+  await getTodosPromise
+  await expect(page.locator('.todo-list li')).toHaveCount(todos.length)
+
+  // enter a new todo item
+  // and confirm the POST /todos request body
+  // is sent correctly
+  const newTodo = 'walk the dog'
+  const postTodoPromise = page.waitForResponse(
+    (response) =>
+      response.url().endsWith('/todos') &&
+      response.request().method() === 'POST',
+  )
+  await page.fill('.new-todo', newTodo)
+  await page.keyboard.press('Enter')
+  await expect(page.locator('.todo-list li')).toHaveCount(todos.length + 1)
+  const postResponse = await postTodoPromise
+  const postResponseBody = await postResponse.json()
+  expect(postResponseBody.title).toBe(newTodo)
 })

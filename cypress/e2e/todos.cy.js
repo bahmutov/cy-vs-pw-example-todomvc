cf0aefc96d56c3bc8b7dad4c469482784c495deb
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2026-02-08 21:59:09 -0500

    initial code

--- cypress/e2e/todos.cy.js
+++ cypress/e2e/todos.cy.js
@@ -5,29 +5,16 @@ describe('Random todos', { viewportHeight: 1000 }, () => {
   beforeEach(function createRandomTodos() {
     // TODO: pass the number of created todos to the main test
     // Tip: use an alias that is immediately available inside the test context function
-    const n = Cypress._.random(0, 10)
-    cy.log(`Creating ${n} random todos`)
-    cy.wrap(n).as('n')
-
     // create `n` random todos and reset the app state with them
     // each todo should have a unique id (could be a number between 100000 and 999999)
     // and a title like "Random todo 1", "Random todo 2", etc.
     // and completed should be false
     // Tip: use "POST /reset" utility API endpoint
-    const todos = Cypress._.times(n, (i) => {
-      return {
-        id: Cypress._.random(1e5, 1e6),
-        title: `Random todo ${i + 1}`,
-        completed: false,
-      }
-    })
-    cy.request('POST', '/reset', { todos })
   })
 
   it('shows the expected number of todos', function () {
     const todos = '.todo-list li'
     cy.visit('/')
     // confirm the app shows the created number of todos
-    cy.get(todos).should('have.length', this.n)
   })
 })
--- pw/todos.spec.js
+++ pw/todos.spec.js
@@ -9,20 +9,8 @@ test.describe('Random todos', () => {
 
   test.beforeEach(async ({ request }) => {
     // Create a random number of todos (0-10)
-    numberOfTodos = Math.floor(Math.random() * 11)
-    await test.step(`Creating ${numberOfTodos} random todos`, async () => {})
-
     // Create todos with unique IDs and titles
-    const todos = Array.from({ length: numberOfTodos }, (_, i) => ({
-      id: Math.floor(Math.random() * 900000) + 100000,
-      title: `Random todo ${i + 1}`,
-      completed: false,
-    }))
-
     // Reset the app state with the created todos
-    await request.post('/reset', {
-      data: { todos },
-    })
   })
 
   test('shows the expected number of todos', async ({ page }) => {
@@ -30,6 +18,5 @@ test.describe('Random todos', () => {
 
     // Confirm the app shows the expected number of todos
     const todoItems = page.locator('.todo-list li')
-    await expect(todoItems).toHaveCount(numberOfTodos)
   })
 })
